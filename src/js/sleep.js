9e00687b4627a6cdca81d22b22620d453f167d42
justorez <alonezero@foxmail.com>
2022-02-26 23:08:55 +0800

    chore: 整理原型链示例

--- src/js/sleep.js
+++ src/js/sleep.js
@@ -1,5 +1,7 @@
-export default function (delay = 1000) {
+function sleep(delay = 1000) {
     return new Promise(resolve => {
         setTimeout(resolve, delay)
     })
-}   
+}
+
+module.exports = sleep
