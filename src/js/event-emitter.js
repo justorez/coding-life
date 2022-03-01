68054517c61d6e96b356eae12dc38f18a89626a5
justorez <alonezero@foxmail.com>
2022-03-01 21:59:10 +0800

    improvement: 优化事件总线

---
+++ src/js/event-emitter.js
@@ -0,0 +1,98 @@
+// 事件总线：发布-订阅模式
+class EventEmitter  {
+    constructor() {
+        this.cache = {}
+    }
+
+    indexOf(name, fn) {
+        return (this.cache[name] || []).findIndex(f => f === fn)
+    }
+
+    /**
+     * 订阅事件
+     * @param {string} name 事件名
+     * @param {function} fn 回调函数
+     */
+    on(name, fn) {
+        if (typeof fn !== 'function') {
+            throw new TypeError('非法 callback 类型')
+        }
+
+        const index = this.indexOf(name, fn)
+        if (index !== -1) {
+            return
+        }
+
+        if (this.cache[name]) {
+            this.cache[name].push(fn)
+        } else {
+            this.cache[name] = [ fn ]
+        }
+    }
+
+    /**
+     * 单次订阅事件
+     * @param {string} name 事件名
+     * @param {function} fn 回调函数
+     */
+    once(name, fn) {
+        fn.__once = true
+        this.on(name, fn)
+    }
+
+    /**
+     * 取消订阅事件
+     * @param {string} name 事件名
+     * @param {function} fn 事件回调函数
+     */
+    off(name, fn) {
+        const index = this.indexOf(name, fn)
+        const tasks = this.cache[name]
+        if (tasks && index >= 0) {
+            tasks.splice(index, 1)
+        }
+    }
+
+    /**
+     * 触发事件
+     * @param {string} name 事件名
+     * @param  {...any} args 其他参数
+     */
+    emit(name, ...args) {
+        this.__emit(name, ...args)
+        this.__emit('*', ...args)
+    }
+
+    __emit(name, ...args) {
+        if (this.cache[name]) {
+            const tasks = this.cache[name].slice()
+            for (const fn of tasks) {
+                fn(...args)
+                fn.__once && this.off(name, fn)
+            }
+        }
+    }
+}
+
+function test() {
+    const eventBus = new EventEmitter()
+    eventBus.on('login', (msg) => {
+        console.log('[login]', msg)
+    })
+    eventBus.on('login', () => {
+        console.log('[login]', new Date().toLocaleString())
+    })
+
+    const fn = () => console.log('无效订阅')
+    eventBus.on('login', fn)
+    eventBus.off('login', fn)
+    eventBus.on('*', (_, num) => {
+        console.log('[*]', num)
+    })
+    eventBus.emit('login', 'I am leo', 18)
+
+    eventBus.once('once-do', console.log)
+    eventBus.emit('once-do', '[once-do]', new Date().toLocaleString())
+    eventBus.emit('once-do', '[once-do]', new Date().toLocaleString())
+}
+test()
