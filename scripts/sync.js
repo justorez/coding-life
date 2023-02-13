b110ac730d356702016f47e7ddd546d0f4eeb8f8
justorez <alonezero@foxmail.com>
2023-02-13 17:24:27 +0800

    script: add universal sync script

---
+++ scripts/sync.js
@@ -0,0 +1,3 @@
+const shell = require('shelljs')
+
+shell.exec('git push -f git@github.com:justorez/coding-life.git master')
--- sync.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem 同步到 GitHub
-git push -f git@github.com:justorez/coding-life.git master
-
-pause
