195dad66b2e9fedc52fee572da0166205fadc4ca
justorez <alonezero@foxmail.com>
2023-02-28 10:27:05 +0800

    refactor: compose function

--- src/handwritten/__test__/compose.test.js
+++ src/handwritten/__test__/compose.test.js
@@ -1,18 +1,34 @@
 const compose = require('../compose')
 
-test('compose test', () => {
-    const fn1 = x => x + 1
-    const fn2 = x => x + 2
-    const fn3 = x => x + 3
-    const fn4 = x => x + 4
+describe('函数组合', () => {
+    test('函数组合功能测试', () => {
+        const fn1 = x => x + 1
+        const fn2 = x => x + 2
+        const fn3 = x => x + 3
+        const fn4 = x => x + 4
+    
+        const c1 = compose(fn1, fn2, fn3, fn4)
+        const c2 = compose(fn2)
+        const c3 = compose()
+    
+        expect(c1(1)).toBe(11)
+        expect(c2).toBe(fn2)
+        expect(c2(1)).toBe(3)
+        expect(c3(1)).toBe(1)
+    })
 
-    const c1 = compose(fn1, fn2, fn3, fn4)
-    expect(c1(1)).toBe(11)
-
-    const c2 = compose(fn2)
-    expect(c2).toBe(fn2)
-    expect(c2(1)).toBe(3)
-
-    const c3 = compose()
-    expect(c3(1)).toBe(1)
+    test('函数组合 this', () => {
+        function fn1(v) {
+            return v + this.val
+        }
+        function fn2(v) {
+            return v + '.'
+        }
+    
+        const obj = {
+            fn: compose(fn1, fn2),
+            val: 'Tim'
+        }
+        expect(obj.fn('Hello! ')).toEqual('Hello! Tim.')
+    })
 })
--- src/handwritten/compose.js
+++ src/handwritten/compose.js
@@ -1,8 +1,26 @@
-function compose(...fn) {
-    if (fn.length === 0) return (arg) => arg
-    return fn.reduce((cur, next) => {
-        return (...args) => next( cur(...args) )
-    })
+// function compose(...args) {
+//     if (args.length === 0) return (arg) => arg
+//     if (args.length === 1) return args[0]
+//     const size = args.length - 1
+//     return function () {
+//         let i = 0
+//         let result = args[i].apply(this, arguments)
+//         while (i++ < size) result = args[i].call(this, result)
+//         return result
+//     }
+// }
+
+/**
+ * 如果实现从右到左执行，可使用 `reduceRight` 或 `fns.reverse`
+ * @param  {...Function} fns 
+ */
+function compose(...fns) {
+    if (fns.length === 0) return (arg) => arg
+    if (fns.length === 1) return fns[0]
+    return function (arg) {
+        return fns.reduce((val, f) => f.call(this, val), arg)
+    }
 }
+
 
 module.exports = compose
