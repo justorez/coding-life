f49f0a8aacac9a4b8fd986c4e5989ba7f04478ec
justorez <alonezero@foxmail.com>
2023-03-21 16:55:23 +0800

    feat: delay function

--- src/js/sleep.js
+++ src/js/sleep.js
@@ -1,7 +1,23 @@
 function sleep(delay = 1000) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         setTimeout(resolve, delay)
     })
 }
 
+/**
+ * @param {Function} func 
+ * @param {number} ms 
+ * @param  {...any} args 
+ */
+function delay(func, ms, ...args) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            Promise.resolve(func(...args))
+                .then(resolve)
+                .catch(reject)
+        }, ms)
+    })
+}
+
+exports.delay = delay
 module.exports = sleep
