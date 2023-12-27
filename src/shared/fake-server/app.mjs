9c802a6e413f7a318270ae1da1e23243b5bc5afa
justorez <alonezero@foxmail.com>
2023-12-27 20:21:08 +0800

    feat(shared): add fake server
    1. fake server for testing upload file
    2. try canvas to draw progress bar

---
+++ src/shared/fake-server/app.mjs
@@ -0,0 +1,39 @@
+import { createServer } from 'http'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+/**
+ * 一个用于测试的 HTTP 服务
+ * - /up/<file.mp4> 二进制文件流上传接口
+ */
+const server = createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' })
+        fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res)
+        return
+    }
+
+    res.setHeader("access-control-allow-origin", "*")
+    res.setHeader("access-control-allow-headers", "*")
+    res.setHeader("access-control-allow-methods", "*")
+
+    if (req.url.includes('/up')) {
+        const filename = decodeURIComponent(path.parse(req.url).base)
+        const savePath = path.join(__dirname, '../temp', filename)
+        req.pipe(fs.createWriteStream(savePath))
+        req.on('end', () => {
+            res.end('File uploaded successfully')
+        })
+        return
+    }
+
+    res.writeHead(400)
+    res.end('Invalid request')
+})
+
+server.listen(3333, () => {
+    console.log('Server running on http://localhost:3333')
+})
---
+++ src/shared/fake-server/example.mjs
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import ProgressBar from 'progress'
+import fs from 'fs'
+
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:3333',
+    maxBodyLength: 2 * 1024 * 1024 * 1024,
+    maxContentLength: 2 * 1024 * 1024 * 1024
+})
+api.interceptors.response.use(
+    (response) => response.data,
+    (error) => Promise.reject(error.message)
+)
+
+function fileUpload() {
+    const filePath = ''
+    const fileData = fs.readFileSync(filePath)
+
+    const bar = new ProgressBar('正在上传 [:bar] :rate/bps :percent :etas', {
+        complete: '=',
+        incomplete: ' ',
+        width: 20,
+        total: fileData.byteLength
+    })
+
+    api.post('/up/', fileData, {
+        onUploadProgress(event) {
+            console.log(event) // node 环境下进度不是真实的
+        },
+        headers: {
+            "Content-Type": 'video/mp4'
+        }
+    }).then(console.log, console.error)
+}
+
+fileUpload()
